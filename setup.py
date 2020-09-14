8544b57469516fcf1487ec07096b522b6b232994
Jesper Baasch-Larsen <jbl@greensteam.com>
2020-09-14 11:11:44 +0200

    added setup script

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rsacsc-py',
+    version='1.0.0',
+    packages=find_packages(),
+    url='https://github.com/itamarhaber/rsacsc-py',
+    install_requires=[
+        'redis',
+    ],
+    author="Itamar Haber",
+    license='MIT License'
+)
